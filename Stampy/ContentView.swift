fad6d1667c240151ee40636c1818359a8cf213d5
Hiromu Nakano <hiromu.nakano.12@gmail.com>
2020-01-26 09:04:05 +0900

    Initial Commit

---
+++ Stampy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stampy
+//
+//  Created by Hiromu Nakano on 2020/01/26.
+//  Copyright © 2020 Hiromu Nakano. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
